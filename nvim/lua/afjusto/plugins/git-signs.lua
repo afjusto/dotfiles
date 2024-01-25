ac7c613a048a384e84e644ad040a7607bdcf71ba
André Justo <andre.justo@amplemarket.com>
2024-01-25 19:22:40 +0000

    replace git-blame with git-signs

--- nvim/lua/afjusto/plugins/git-signs.lua
+++ nvim/lua/afjusto/plugins/git-signs.lua
@@ -10,6 +10,22 @@ return {
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
 			},
+			current_line_blame = true,
+			current_line_blame_opts = {
+				delay = 500,
+			},
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				map("n", "]c", gs.next_hunk, { desc = "Next Git Hunk" })
+				map("n", "[c", gs.prev_hunk, { desc = "Prev Git Hunk" })
+			end,
 		})
 	end,
 }
