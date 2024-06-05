d223fbe7078262f01d3530a0976844620b128ec6
André Justo <andre.justo@amplemarket.com>
2024-06-05 11:27:02 +0100

    improve completions

--- nvim/lua/afjusto/plugins/nvim-cmp.lua
+++ nvim/lua/afjusto/plugins/nvim-cmp.lua
@@ -13,6 +13,7 @@ return {
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 		local lspkind = require("lspkind")
+		lspkind.init({})
 
 		require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -42,23 +43,16 @@ return {
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+				["<C-c>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
+				{ name = "nvim_lsp" },
 			}),
-			-- configure lspkind for vs-code like pictograms in completion menu
-			formatting = {
-				format = lspkind.cmp_format({
-					maxwidth = 50,
-					ellipsis_char = "...",
-				}),
-			},
 		})
 	end,
 }
