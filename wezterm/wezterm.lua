863fee1d1295d07ec58a9927338bd2ee275311a0
André Justo <andre.justo@amplemarket.com>
2024-11-24 01:04:35 +0000

    goodbye kitty, hello wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,56 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.set_environment_variables = {
+	PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
+}
+
+config.color_scheme = "Tokyo Night Storm"
+
+config.font = wezterm.font("FiraCode Nerd Font Mono", { weight = "Medium" })
+config.font_size = 14.0
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.keys = {
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = wezterm.action.SendString("\x1bb"),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = wezterm.action.SendString("\x1bf"),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = wezterm.action({ SendString = "\x1bOH" }),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = wezterm.action({ SendString = "\x1bOF" }),
+	},
+	{
+		key = ",",
+		mods = "SUPER",
+		action = wezterm.action.SpawnCommandInNewTab({
+			cwd = wezterm.home_dir,
+			args = { "nvim", wezterm.config_file },
+		}),
+	},
+}
+
+config.mouse_bindings = {
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "SUPER",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
+}
+
+return config
