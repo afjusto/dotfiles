5e359b1f42d7bb61ceedb01b3191fa861887a31e
André Justo <>
2025-12-13 12:11:10 +0000

    Revert "replace nvim-cmp with blink"
    This reverts commit 88c5ea8503346e38ede4b7b8c33bcd6902947a35.

--- nvim/lua/custom/plugins/completions.lua
+++ nvim/lua/custom/plugins/completions.lua
@@ -1,98 +1,119 @@
 return {
-  'saghen/blink.cmp',
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
   dependencies = {
-    'onsails/lspkind.nvim',
+    -- Snippet Engine & its associated nvim-cmp source
     {
-      'rafamadriz/friendly-snippets',
-      config = function()
-        require('luasnip.loaders.from_vscode').lazy_load()
-      end,
+      'jdrupal-dev/css-vars.nvim',
+      opts = {},
     },
-    { 'L3MON4D3/LuaSnip', version = 'v2.*' },
-    'jdrupal-dev/css-vars.nvim',
-  },
-  version = '1.*',
+    {
+      'L3MON4D3/LuaSnip',
+      build = (function()
+        -- Build Step is needed for regex support in snippets.
+        -- This step is not supported in many windows environments.
+        -- Remove the below condition to re-enable on windows.
+        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+          return
+        end
+        return 'make install_jsregexp'
+      end)(),
+      dependencies = {
+        -- `friendly-snippets` contains a variety of premade snippets.
+        --    See the README about individual language/framework/plugin snippets:
+        --    https://github.com/rafamadriz/friendly-snippets
+        {
+          'rafamadriz/friendly-snippets',
+          config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+          end,
+        },
+      },
+    },
+    'saadparwaiz1/cmp_luasnip',
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
+    -- Adds other completion capabilities.
+    --  nvim-cmp does not ship with all sources by default. They are split
+    --  into multiple repos for maintenance purposes.
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+  },
   opts = {
-    keymap = {
-      preset = 'enter',
-      ['<C-k>'] = { 'select_prev', 'fallback' },
-      ['<C-j>'] = { 'select_next', 'fallback' },
-      ['<C-l>'] = { 'snippet_forward', 'fallback' },
-      ['<C-h>'] = { 'snippet_backward', 'fallback' },
-      ['<C-space>'] = { 'show' },
-      ['<Tab>'] = false,
+    performance = {
+      debounce = 60,
+      throttle = 30,
+      fetching_timeout = 500,
     },
-
-    appearance = {
-      nerd_font_variant = 'mono',
-    },
-
-    completion = {
-      documentation = { auto_show = true, auto_show_delay_ms = 0, window = { border = 'rounded' } },
-      menu = {
-        auto_show = false,
-        border = 'rounded',
-        draw = {
-          columns = {
-            { 'kind_icon', 'label', gap = 1 },
-            { 'kind' },
-          },
-          components = {
-            kind_icon = {
-              text = function(item)
-                local kind = require('lspkind').symbol_map[item.kind] or ''
-                return kind .. ' '
-              end,
-              highlight = 'CmpItemKind',
-            },
-            label = {
-              text = function(item)
-                return item.label
-              end,
-              highlight = 'CmpItemAbbr',
-            },
-            kind = {
-              text = function(item)
-                return item.kind
-              end,
-              highlight = 'CmpItemKind',
-            },
-          },
-        },
-      },
-      ghost_text = { enabled = false, show_with_menu = false },
-    },
-    signature = { enabled = true, window = { border = 'rounded' } },
-
-    sources = {
-      default = { 'lsp', 'path', 'css_vars', 'snippets', 'buffer' },
-
-      providers = {
-        css_vars = {
-          name = 'css-vars',
-          module = 'css-vars.blink',
-        },
-        lsp = {
-          min_keyword_length = 0,
-        },
-        path = {
-          min_keyword_length = 0,
-        },
-        buffer = {
-          min_keyword_length = 0,
-        },
-        snippets = {
-          min_keyword_length = 2,
-        },
-      },
-    },
-
-    snippets = { preset = 'luasnip' },
-
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
   },
-  opts_extend = { 'sources.default' },
+  config = function()
+    -- See `:help cmp`
+    local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+    luasnip.config.setup {}
+
+    cmp.setup {
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      formatting = {
+        format = function(entry, item)
+          local widths = {
+            abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+            menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+          }
+
+          for key, width in pairs(widths) do
+            if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+              item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. '…'
+            end
+          end
+
+          return item
+        end,
+      },
+      completion = { completeopt = 'menu,menuone,noselect' },
+      mapping = cmp.mapping.preset.insert {
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<CR>'] = cmp.mapping.confirm(),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-c>'] = cmp.mapping.abort(),
+
+        ['<C-l>'] = cmp.mapping(function()
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          end
+        end, { 'i', 's' }),
+        ['<C-h>'] = cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          end
+        end, { 'i', 's' }),
+
+        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      },
+      sources = {
+        {
+          name = 'lazydev',
+          -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+          group_index = 0,
+        },
+        { name = 'nvim_lsp', keyword_length = 0 },
+        { name = 'css_vars' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+      },
+    }
+  end,
 }
--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -102,12 +102,11 @@ return {
       end,
     })
 
-    --  LSP servers and clients are able to communicate to each other what features they support.
+    -- LSP servers and clients are able to communicate to each other what features they support.
     --  By default, Neovim doesn't support everything that is in the LSP specification.
-    --  When you add blink-cmp, luasnip, etc. Neovim now has *more* capabilities.
+    --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-    local original_capabilities = vim.lsp.protocol.make_client_capabilities()
-    local capabilities = require('blink.cmp').get_lsp_capabilities(original_capabilities)
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     -- Enable the following language servers
